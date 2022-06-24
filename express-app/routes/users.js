c3a5a2ddbd32b3a687faff108a9b5e6859cdf6c0
Brandon Poletti <brandon.poletti@gmail.com>
2022-06-24 15:05:03 -0400

    more node js with jade

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/detail', function(req, res, next) {
+  res.send('detail');
+});
+
+//localhost:8000/users/detail
+
 module.exports = router;
